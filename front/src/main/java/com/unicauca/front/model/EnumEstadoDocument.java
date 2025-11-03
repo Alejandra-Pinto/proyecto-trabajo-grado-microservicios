997534fc027cf80dc5a29de162a598b6154ecc01
Juliana <mariajulisanchez@unicauca.edu.co>
2025-11-03 01:33:49 -0500

    Cambios en los enum y document

---
+++ front/src/main/java/com/unicauca/front/model/EnumEstadoDocument.java
@@ -0,0 +1,10 @@
+package com.unicauca.front.model;
+
+public enum EnumEstadoDocument {
+    PRIMERA_REVISION,
+    SEGUNDA_REVISION,
+    TERCERA_REVISION,
+    ACEPTADO,
+    NO_ACEPTADO,
+    RECHAZADO
+}
